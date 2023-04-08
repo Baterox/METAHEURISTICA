c7cad63e3e5a90b150eac9c10f072a052c01f280
Baterox <rodrigo.ordenes.a@gmail.com>
2023-04-08 15:52:35 -0400

    tarea1

--- TAREA 1/nonogram_resolver_using_forward_checking.py
+++ TAREA 1/nonogram_resolver_using_forward_checking.py
@@ -1,30 +1,18 @@
 import itertools
+import time
 
 class NonogramSolver():
     def __init__(self, N:int, row_constraits:list, col_constraits:list):
         self.N = N
         self.row_constraits = row_constraits
         self.col_constraits = col_constraits
-        self.solution = [[0, 1]]*N**2
+        self.solution = [None]*N**2
         self.domain_lenght = 2
 
     def check_constraints(self, current_solution:list, constraits:list) -> bool:
         return [len(list(g)) for k, g in itertools.groupby(current_solution) if k == 1] == constraits
 
-    def get_current_index(self, current_solution:list) -> int:
-        current_solution_len = [len(domain) for domain in current_solution]
-        try:
-            return current_solution_len.index(self.domain_lenght)
-        except:
-            return self.N**2
-
-    def check_non_empty_domains(self, selected_index:int, new_solution:list) -> bool:
-        for i in range(selected_index + 1, len(new_solution)):
-            if not new_solution[i]:
-                return False
-        return True
-    
-    def viable(self, grid:list, index:int, value:int) -> bool:
+    def viable(self, current_solution:dict, index:int, value:int) -> bool:
         index_row = (index // self.N)
         row_start =  index_row * self.N
         col_start = index_col = index % self.N
@@ -33,16 +21,9 @@ class NonogramSolver():
         col = []
 
         for i in range(self.N):
-            if row_start+i < len(grid):
-                row.append(grid[row_start+i])
-            else:
-                row.append(None)
+            row.append(current_solution[row_start+i])
+            col.append(current_solution[col_start+10*i])
         
-            if col_start+10*i < len(grid):
-                col.append(grid[col_start+10*i])
-            else:
-                col.append(None) 
-
         row[index_col] = value
         col[index_row] = value
 
@@ -51,38 +32,52 @@ class NonogramSolver():
             return False
         return True
 
-    def forward_checking(self, solution:list) -> list:
-        selected_index = self.get_current_index(solution)
-        if selected_index == len(solution):
+    def forward_checking(self, solution:list, index, selected_index) -> list:
+        if selected_index == self.N**2:
             return solution
 
-        for value in solution[selected_index]:
-            grid = [solution[i][0] for i in range(selected_index)]
+        for value in [0,1]:
+            # grid = [solution[i] for i in range(selected_index)]
 
+            current_solution = solution.copy()
+            if self.viable(current_solution, selected_index, value):
+                current_solution[selected_index] = value
 
-            if self.viable(grid, selected_index, value):
-                new_solution = [domain.copy() for domain in solution]
-                new_solution[selected_index] = [value]
-
-                # for i in range(selected_index + 1, len(solution)):
+                # for i in range(self.N**2):
                 #     consistents = []
-                #     for v in solution[i]:
-                #         if self.viable(grid,i,v):
-                #             consistents.append(v)
-                #     new_solution[i] = consistents
+                #     for future_value in [0, 1]:
+                #         if self.viable(current_solution, i, future_value):
+                #             consistents.append(future_value)
+                    
+                #     if len(consistents) == 0:
+                #         return None
+                    
+                #     current_solution[i] = consistents
 
-                if self.check_non_empty_domains(selected_index, new_solution):
-                    result = self.forward_checking(new_solution)
-                    if result:
-                        return result
+                result = self.forward_checking(current_solution, index, selected_index+1)
+                if result:
+                    return result
         return None
 
     def puzzle_solver(self) -> list:
-        new_domains = self.forward_checking(self.solution)
+        i = [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+            40, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+            60, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+            50, 61, 62, 63, 64, 65, 66, 67, 68, 69,
+            70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+            80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+            90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+        ]
+        new_domains = self.forward_checking(self.solution, i, 0)
+
         #print( new_domains)
 
         if new_domains:
-            grid = {i: new_domains[i][0] for i in range(len(new_domains))}
+            grid = {i: new_domains[i] for i in range(len(new_domains))}
             grid = [[grid[row * self.N + col] for col in range(self.N)] for row in range(self.N)]
             return grid
         else:
@@ -95,12 +90,13 @@ if __name__ == "__main__":
         [[4],[2],[7],[3, 4],[7, 2],[7, 2],[3, 4],[7],[2],[4]]
     )
 
+    start_time = time.perf_counter()
     solution = nonograma.puzzle_solver()
+    end_time = time.perf_counter()
 
     if solution:
         print("\n".join(["".join(["██" if j == 1 else "░░" for j in i]) for i in solution]))
     else:
         print("No se ha encontrado una solución para este puzzle")
 
-
-
+    print(f"Tiempo de ejecución: {end_time - start_time:.4f} segundos")
--- TAREA 1/problem.py
+++ TAREA 1/problem.py
@@ -1,46 +1,4 @@
-import itertools
-
-def get_row_indices(index, N):
-    row = index // N
-    return [row * N + i for i in range(N)]
-
-def get_col_indices(index, N):
-    col = index % N
-    return [col + i * N for i in range(N)]
-
-def check_constraint(sequence, clues):
-    count = [len(list(g)) for k, g in itertools.groupby(sequence) if k == 1]
-    return count == clues
-
-def is_consistent(assignment, index, value, row_clues, col_clues, N):
-    row_indices = get_row_indices(index, N)
-    col_indices = get_col_indices(index, N)
-    
-    row = [assignment.get(i, None) for i in row_indices]
-    col = [assignment.get(i, None) for i in col_indices]
-    
-    row[index % N] = value
-    col[index // N] = value
-    
-    if None not in row and not check_constraint(row, row_clues[index // N]):
-        return False
-
-    if None not in col and not check_constraint(col, col_clues[index % N]):
-        return False
-
-    return True
-
-def select_variable(domains):
-    min_remaining_values = float('inf')
-    selected_index = -1
-
-    for i, domain in enumerate(domains):
-        if 1 < len(domain) < min_remaining_values:
-            min_remaining_values = len(domain)
-            selected_index = i
-    return selected_index if selected_index != -1 else len(domains)
-
-def forward_checking(domains, N, row_clues, col_clues, node_count, backtrack_count):
+def forward_checking(domains, width, height, row_clues, col_clues, node_count, backtrack_count):
     selected_index = select_variable(domains)
 
     if selected_index == len(domains):
@@ -51,69 +9,18 @@ def forward_checking(domains, N, row_clues, col_clues, node_count, backtrack_cou
     for value in domains[selected_index]:
         assignment = {i: domains[i][0] for i in range(selected_index)}
 
-        if is_consistent(assignment, selected_index, value, row_clues, col_clues, N):
+        if is_consistent(assignment, selected_index, value, row_clues, col_clues, width, height):
             new_domains = [domain.copy() for domain in domains]
             new_domains[selected_index] = [value]
 
             for i in range(selected_index + 1, len(domains)):
-                new_domains[i] = [v for v in domains[i] if is_consistent(assignment, i, v, row_clues, col_clues, N)]
+                new_domains[i] = [v for v in domains[i] if is_consistent(assignment, i, v, row_clues, col_clues, width, height)]
 
             if all(new_domains[i] for i in range(selected_index + 1, len(domains))):
-                result, new_node_count, new_backtrack_count = forward_checking(new_domains, N, row_clues, col_clues, node_count, backtrack_count)
+                result, new_node_count, new_backtrack_count = forward_checking(new_domains, width, height, row_clues, col_clues, node_count, backtrack_count)
                 if result:
                     return result, new_node_count, new_backtrack_count
         else:
             backtrack_count += 1
 
     return None, node_count, backtrack_count
-
-def solve_nonogram(row_clues, col_clues):
-    N = len(row_clues)
-    domains = [[0, 1] for _ in range(N**2)]
-    new_domains, node_count, backtrack_count = forward_checking(domains, N, row_clues, col_clues, 0, 0)
-
-    if new_domains:
-        assignment = {i: new_domains[i][0] for i in range(len(new_domains))}
-        grid = [[assignment[row * N + col] for col in range(N)] for row in range(N)]
-        return grid, node_count, backtrack_count
-    else:
-        print("No solution found")
-
-def main():
-
-    row_clues = [
-        [4],
-        [8],
-        [10],
-        [1, 1, 2, 1, 1],
-        [1, 1, 2, 1, 1],
-        [1, 6, 1],
-        [6],
-        [2, 2],
-        [4],
-        [2]
-    ]
-
-    col_clues = [
-        [4],
-        [2],
-        [7],
-        [3, 4],
-        [7, 2],
-        [7, 2],
-        [3, 4],
-        [7],
-        [2],
-        [4]
-    ]
-
-    solution, node_count, backtrack_count = solve_nonogram(row_clues, col_clues)
-
-    if solution:
-        for row in solution:
-            print("".join("# " if cell == 1 else ". " for cell in row))
-
-    print(f"Nodos generados: {node_count}")
-    print(f"Nodos con backtracking: {backtrack_count}")
-
-main()
